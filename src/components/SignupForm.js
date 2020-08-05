d49f109004105cc1b836748c864b997617cf0481
Meghan Adkins <meghandadkins@gmail.com>
2020-08-05 15:01:23 -0400

    initial set up of signup form component

---
+++ src/components/SignupForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSignupForm = styled.div``;
+
+export const SignupForm = () => {
+  return (
+    <StyledSignupForm>
+      <form>
+        <p>Hello from Signup!</p>
+      </form>
+    </StyledSignupForm>
+  );
+};
