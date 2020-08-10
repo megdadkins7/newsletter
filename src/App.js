ff3b66998d5a1da64979cebae6b109899c5e251a
Meghan Adkins <meghandadkins@gmail.com>
2020-08-10 13:31:42 -0400

    added styles to form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 //components
 import { SignupForm } from "./components/SignupForm";
 
+const StyledHeader = styled.h1`
+  text-align: center;
+  margin-bottom: -20px;
+  text-transform: uppercase;
+  font-weight: 400;
+  color: #783be3;
+`;
+
 function App() {
   return (
     <div>
-      Hello from app
+      <StyledHeader>Subscribe to my Newsletter!</StyledHeader>
       <SignupForm />
     </div>
   );
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,13 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledSignupForm = styled.div``;
+const StyledSignupForm = styled.div`
+  form {
+    max-width: 420px;
+    margin: 50px auto;
+  }
+  .MessageInput {
+    color: white;
+    font-family: Helvetica, Arial, sans-serif;
+    font-weight: 500;
+    font-size: 18px;
+    border-radius: 5px;
+    line-height: 22px;
+    background-color: transparent;
+    border: 2px solid #915ee8;
+    transition: all 0.3s;
+    padding: 13px;
+    margin-bottom: 15px;
+    width: 100%;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  .MessageInput:focus {
+    border: 2px solid #6c29e0;
+  }
+  [type="submit"] {
+    font-family: Helvetica, Arial, sans-serif;
+    width: 100%;
+    background: #915ee8;
+    border-radius: 5px;
+    border: 0;
+    cursor: pointer;
+    color: white;
+    font-size: 24px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    transition: all 0.3s;
+    margin-top: -4px;
+    font-weight: 700;
+  }
+  [type="submit"]:hover {
+    background: #783be3;
+  }
+`;
 
 export const SignupForm = () => {
   return (
     <StyledSignupForm>
       <form>
-        <p>Hello from Signup!</p>
+        <input
+          className="MessageInput"
+          type="email"
+          placeholder="email address"
+        />
+        <button type="submit">Subscribe</button>
       </form>
     </StyledSignupForm>
   );
